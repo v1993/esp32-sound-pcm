d2a05e18be1107327b5b9aa6f0c9d1cbe36171cf
v1993 <v19930312@gmail.com>
2018-01-05 21:12:59 +0300

    Working code

---
+++ soundProviderPcm.cpp
@@ -0,0 +1,21 @@
+#include <soundProviderPcm.h>
+
+SoundProviderPcm::SoundProviderPcm(const SoundData *dataarg, SoundPos lenarg, unsigned long int freqarg) {
+	data = dataarg;
+	freq = freqarg;
+	len = lenarg;
+}
+
+SoundProviderPcm::~SoundProviderPcm() {
+	provider_stop();
+}
+
+unsigned long int SoundProviderPcm::getFrequency() {
+	return freq;
+}
+
+void SoundProviderPcm::task_code() {
+	for (SoundPos i = 0; i < len; i++) {
+		postSample(data[i]);
+	}
+}
